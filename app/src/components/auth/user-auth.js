d2184fd89b1f3274c4f2bb20ce9a77b011ab1524
Danielle H <daheber@gmail.com>
2016-09-06 20:49:18 -0700

    integrated angular messages for more consistent UI

--- app/src/components/auth/user-auth.js
+++ app/src/components/auth/user-auth.js
@@ -1,6 +1,6 @@
 export default {
   template: `
-	<div>
+	<md-dialog-content>
 		<label>
 			<input type="radio" ng-model="$ctrl.action" value="signin">
 			Sign In
@@ -9,10 +9,9 @@ export default {
 			<input type="radio" ng-model="$ctrl.action" value="signup">
 			Sign Up
 		</label>
-	</div>
 	<signin ng-if="$ctrl.action==='signin'" success="$ctrl.success()"></signin>
 	<signup ng-if="$ctrl.action==='signup'" success="$ctrl.success()"></signup>
-	`,
+  `,
   bindings: {
     success: '&'
   },
