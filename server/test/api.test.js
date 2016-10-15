bbb6f544bd0351cada83bbcbb6a2156655186163
Danielle H <daheber@gmail.com>
2016-10-15 16:41:19 -0700

    images routes end to end tests

--- server/test/api.test.js
+++ server/test/api.test.js
@@ -8,8 +8,8 @@ const app = require('../app');
 
 describe('api end to end', ()=>{
   // clears out the db
-  before(done => {
-    const drop = () => connection.db.dropDatabase(done);
+  before(done=>{
+    const drop = ()=>connection.db.dropDatabase(done);
     if (connection.readyState === 1) drop();
     else connection.on('open', drop);
   });
@@ -26,6 +26,12 @@ describe('api end to end', ()=>{
     title: 'This is a test album'
   };
 
+  const testImage = {
+    title: 'test',
+    description: 'desc',
+    link: 'www.somthing.com'
+  };
+
   let token = '';
 
   describe('User creation/auth', ()=>{
@@ -33,7 +39,7 @@ describe('api end to end', ()=>{
     it('creates a new user and generates a token', done=>{
       request.post('/api/signup')
         .send(testUser)
-        .then(res => {
+        .then(res=>{
           testUser._id = res.body.auth.id;
 
           assert.ok(res.body.auth.id);
@@ -46,7 +52,7 @@ describe('api end to end', ()=>{
     it('permits signin with correct username/password', done=>{
       request.post('/api/signin')
         .send({ username: testUser.username, password: testUser.password })
-        .then(res => {
+        .then(res=>{
           token = res.body.auth.token;
           
           assert.ok(res.body.auth.id);
@@ -59,7 +65,7 @@ describe('api end to end', ()=>{
     it('errors on signin with incorrect username', done=>{
       request.post('/api/signin')
         .send({ username: 'fake user', password: testUser.password })
-        .then(() => done('status should not be 200'))
+        .then(()=> done('status should not be 200'))
         .catch(res=>{
           assert.equal(res.status, 400);
           done();
@@ -70,7 +76,7 @@ describe('api end to end', ()=>{
     it('errors with incorrect password', done=>{
       request.post('/api/signin')
         .send({ username: testUser.username, password: 'fake password' })
-        .then(() => done('status should not be 200'))
+        .then(()=> done('status should not be 200'))
         .catch(res=>{
           assert.equal(res.status, 400);
           done();
@@ -81,7 +87,7 @@ describe('api end to end', ()=>{
     it('errors when signing up using a username that exists in the system', done=>{
       request.post('/api/signup')
         .send({ email: 'testemail@email.com', username: 'testUser', password: '123'})
-        .then(() => done('status should not be 200'))
+        .then(()=> done('status should not be 200'))
         .catch(res=>{
           assert.equal(res.status, 500);
           done();
@@ -95,7 +101,7 @@ describe('api end to end', ()=>{
         request.post(`/api/albums/${testUser._id}`)
           .set({Authorization: token})
           .send(testAlbum)
-          .then(res =>{
+          .then(res=>{
             testAlbum.__v = 0;
             testAlbum.updatedAt = res.body.updatedAt;
             testAlbum.createdAt = res.body.createdAt;
@@ -112,7 +118,7 @@ describe('api end to end', ()=>{
       it('lists albums owned by testUser', done=>{
         request.get(`/api/albums/${testUser._id}`)
           .set({Authorization: token})
-          .then(res =>{
+          .then(res=>{
             assert.deepEqual(res.body[0], testAlbum);
             done();
           })
@@ -123,7 +129,7 @@ describe('api end to end', ()=>{
         request.put(`/api/albums/${testAlbum._id}`)
           .set({Authorization: token})
           .send({title: 'Updated title'})
-          .then(res =>{
+          .then(res=>{
             console.log(res.body.title);
             assert.equal(res.body.title, 'Updated title');
             done();
@@ -134,7 +140,7 @@ describe('api end to end', ()=>{
       it('deletes the album', done=>{
         request.delete(`/api/albums/${testAlbum._id}`)
           .set({Authorization: token})
-          .then(res =>{
+          .then(res=>{
             assert.equal(res.body._id, testAlbum._id);
             done();
           })
@@ -143,9 +149,50 @@ describe('api end to end', ()=>{
 
     });
 
-    // describe('image CRUD', ()=>{
+    describe('image create/update/delete', ()=>{
 
-    // });
+      it('creates a new image', done=>{
+        request.post(`/api/images/${testUser._id}`)
+          .set({Authorization: token})
+          .send(testImage)
+          .then(res=>{
+            testImage._id = res.body._id;
+            testImage.user = res.body.user;
+            testImage.updatedAt = res.body.updatedAt;
+            testImage.createdAt = res.body.createdAt;
+            testImage.__v = 0;
+  
+            assert.deepEqual(res.body, testImage);
+            done();
+          })
+          .catch(done);
+      });
+
+      it('updates testImage title', done=>{
+        request.put(`/api/images/${testImage._id}`)
+          .set({Authorization: token})
+          .send({title: 'hello world'})
+          .then(res=>{
+            testImage.title = res.body.title;
+            assert.equal(res.body.title, 'hello world');
+            done();
+          })
+          .catch(done);
+      });
+
+      it('deletes testImage', done=>{
+        request.delete(`/api/images/${testImage._id}`)
+          .set({Authorization: token})
+          .then(res=>{
+            assert.equal(res.body.title, testImage.title);
+            assert.equal(res.body.description, testImage.description);
+            assert.equal(res.body.link, testImage.link);
+            done();
+          })
+          .catch(done);
+      });
+      
+    });
 
   });
 
